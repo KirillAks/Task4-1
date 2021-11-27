333880189a80c75d7581fe537666ecabc2c390f0
Пользователь <@DESKTOP-O54IC2D>
2021-11-27 16:28:55 +0300

    Добавьте файлы проекта.

---
+++ Task4-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N>0");
+            int n=Convert.ToInt32(Console.ReadLine());
+            for (int i = 1, j = 0; i <= n; i++)
+            {
+                Console.WriteLine("Квадрат числа {0} равен {1}", i, j += 2 * i - 1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
